bc57ec81c34dc582e10552c4fa628de39c4328d7
Sachin Prabhu <sachin.prabhu@gmail.com>
2020-07-12 23:06:36 +0100

    Change EStep for Hemera

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -928,7 +928,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 409 }
 
 /**
  * Default Max Feed Rate (mm/s)
