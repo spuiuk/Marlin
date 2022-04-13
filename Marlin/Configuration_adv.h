45d99c5dec212841e949e2a502c17fc7b12c7a1d
Sachin Prabhu <sprabhu@redhat.com>
2022-04-13 16:04:30 +0100

    Increase Z axis stepper motor current
    The driver runs dual z axis stepper motors and needs more current to
    run.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2837,7 +2837,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       580
+    #define Z_CURRENT       800
     #define Z_CURRENT_HOME  Z_CURRENT / 2
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
